679c8ecf68ece83573775b849668b138862e11fa
dacryorrhoea <dacryorrhoea@gmail.com>
2025-02-28 10:37:52 -0800

    pip

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="main">
+    <!-- Отображение карточки, если клиент Telegram и API обновлены -->
     <v-card v-if="is_telegram_client && is_telegram_api_updated" class="mx-auto" max-width="600">
     </v-card>
+    <!-- Компонент с сообщением о требованиях -->
     <RequirementsMessage :is-telegram-client="is_telegram_client" :is-telegram-api-updated="is_telegram_api_updated" />
   </div>
 </template>
 
 <script>
+// Импорт вспомогательных функций и компонентов
 import { detectCodeType, prepareUrl, prepareCoordinate, prepareWifi, prepareVCard } from './helpers';
 import AppSettings from "./components/AppSettings.vue"
 import CardUrl from "./components/CardUrl.vue";
@@ -28,54 +31,55 @@ export default {
   },
   data() {
     return {
-      is_telegram_client: false,
-      is_telegram_api_updated: false,
-      last_code: null,
-      show_history: true,
-      // Cloud storage
-      cloud_storage_keys: [],
-      cloud_storage_values: {},
-      enriched_values: {},
-      is_continuous_scan: false,
-      // Set the first element to expanded by default
-      expanded_panels: [0],
+      is_telegram_client: false, // Флаг наличия клиента Telegram
+      is_telegram_api_updated: false, // Флаг обновлённого API Telegram
+      last_code: null, // Последний отсканированный QR-код
+      show_history: false, // Отображение истории
+      cloud_storage_keys: [], // Ключи облачного хранилища
+      cloud_storage_values: {}, // Значения облачного хранилища
+      enriched_values: {}, // Обогащённые значения QR-кодов
+      is_continuous_scan: false, // Флаг непрерывного сканирования
+      expanded_panels: [0], // Развернутые панели (по умолчанию первая)
     };
   },
   created() {
-    // Binding function to the events types
-    this.TMA.MainButton.setText("Scan QR code");
+    // Настройка кнопки главного экрана
+    this.TMA.MainButton.setText("Сканировать QR-код");
     this.TMA.onEvent('qrTextReceived', this.processQRCode);
     this.TMA.onEvent('mainButtonClicked', this.mainButtonClicked);
-
-    // platform not updated if version is not 6.9 or greater
+    
+    // Проверка версии API Telegram
     this.is_telegram_api_updated = this.TMA.isVersionAtLeast('6.9');
     if (this.TMA.platform != "unknown") {
       this.is_telegram_client = true;
     }
     if (this.is_telegram_client && this.is_telegram_api_updated) {
-      this.TMA.MainButton.show();
-      this.loadStorage();
+      this.TMA.MainButton.show(); // Показываем кнопку
+      // this.loadStorage(); // Загружаем данные из облачного хранилища
     }
   },
   mounted() {
-    // Mini app ready
+    // Уведомляем Telegram Mini App о готовности
     this.TMA.ready();
   },
   methods: {
-    // Cloud Storage methods
+    // Загрузка данных из облачного хранилища
     loadStorage() {
       this.TMA.CloudStorage.getKeys(this.processKeys);
     },
+    
+    // Обработка полученных ключей из облачного хранилища
     processKeys(error, data) {
       if (error) {
         this.TMA.showAlert(error);
         return;
       }
-      //sort timestamps in descending order
-      data.sort((a, b) => b - a);
+      data.sort((a, b) => b - a); // Сортировка по убыванию времени
       this.cloud_storage_keys = data;
       this.TMA.CloudStorage.getItems(data, this.processItems);
     },
+    
+    // Обработка полученных значений из облачного хранилища
     processItems(error, data) {
       if (error) {
         this.TMA.showAlert(error);
@@ -84,17 +88,8 @@ export default {
       this.cloud_storage_values = data;
       this.enrichValues(data);
     },
-    removeKey(key) {
-      //TODO clean the enriched_values
-      for (var index = 0; index < this.cloud_storage_keys.length; index++) {
-        if (this.cloud_storage_keys[index] === key) {
-          this.cloud_storage_keys.splice(index, 1);
-          delete this.cloud_storage_values[key];
-          break;
-        }
-      }
-      this.TMA.CloudStorage.removeItem(key);
-    },
+    
+    // Обогащение значения QR-кода доп. информацией
     enrichValue(key) {
       this.enriched_values[key] = {};
       const code_type = detectCodeType(this.cloud_storage_values[key]);
@@ -112,113 +107,31 @@ export default {
         this.enriched_values[key]['info'] = this.cloud_storage_values[key];
       }
     },
+    
     enrichValues(data) {
-      for (var key in data) {
+      for (let key in data) {
         this.enrichValue(key);
       }
     },
-    addToStorage(value) {
-      // generate a key based on the timestamp
-      const timestamp = new Date().getTime();
-      this.TMA.CloudStorage.setItem(timestamp, value);
-      // convert timestamp in string and add it to the array
-      this.cloud_storage_keys.unshift(timestamp.toString());
-      this.cloud_storage_values[timestamp] = value;
-      return timestamp;
-    },
-    // Event Callback
-    mainButtonClicked() {
-      this.showQRScanner();
-    },
-    // QR scanner functions
-    showQRScanner() {
-      // Sets QR message
-      let par = {
-        text: ""
-      };
-      if (this.is_continuous_scan) {
-        par['text'] = "Continuous scan enabled.";
-      }
-      this.TMA.showScanQrPopup(par);
-    },
+    
+    // Обработка полученного QR-кода
     processQRCode(data) {
-      // This function is called every time the scanner recognises a QR code
-      // check if the QR code text is longer than 4096 characters
       if (data.data.length > 4096) {
-        this.TMA.showAlert('Error cannot store QR codes longer than 4096 characters');
+        this.TMA.showAlert('Ошибка: QR-код слишком длинный');
         return;
       }
-      // avoids to scan the same code twice in continuous scan mode
-      if (data.data == this.last_code) {
-        return;
-      }
-      this.last_code = data.data;
+  
       this.hapticImpact();
-      let key = this.addToStorage(data.data);
-      this.enrichValue(key);
 
-      // Отправка отсканированного QR-кода обратно в Telegram-бот
-      this.TMA.sendData(data.data);
-
-      // Force to go back to the history screen if setting screen is open
-      this.show_history = true;
-      // Force to display the last element scanned
-      this.expanded_panels = [0];
-
-      if (!this.is_continuous_scan) {
-        this.TMA.closeScanQrPopup();
-        // Закрытие мини-приложения
-        this.TMA.close();
-      }
+      this.TMA.sendData(data.data); // Отправка данных в Telegram-бот
+      this.TMA.closeScanQrPopup();
+      this.TMA.close(); // Закрытие мини-приложения
     },
+    
+    // Вибрация при обнаружении QR-кода
     hapticImpact() {
-      // makes the phone vibrate when QR is detected
       this.TMA.HapticFeedback.impactOccurred("rigid");
       this.TMA.HapticFeedback.impactOccurred("heavy");
-    },
-    // Utils
-    formattedDate(timestamp) {
-      // Create a Date object from the timestamp
-      const date = new Date(parseInt(timestamp));
-
-      // Extract day, month, year, hour, and minute components
-      const day = date.getDate();
-      const month = date.getMonth() + 1; // Months are zero-based, so add 1
-      const year = date.getFullYear();
-      const hour = date.getHours();
-      const minute = date.getMinutes();
-      const second = date.getSeconds();
-      // Format the date as "dd/mm/yyyy hh:mm:ss"
-      const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year} ${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
-      return formattedDate;
-    },
-    getIconFromType(key) {
-      // check if key exists
-      if (!this.enriched_values[key]) {
-        return "mdi-text-box";
-      }
-      // check it key type exists
-      if (!this.enriched_values[key]['type']) {
-        return "mdi-text-box";
-      }
-      let type = this.enriched_values[key]['type'];
-      if (type == "geo") {
-        return "mdi-map-marker-outline";
-      } else if (type == "wifi") {
-        return "mdi-wifi";
-      } else if (type == "vcard") {
-        return "mdi-account";
-      } else if (type == "url") {
-        return "mdi-link";
-      } else {
-        return "mdi-text-box";
-      }
-    },
-    limitLength(value, max_length) {
-      if (value.length <= max_length) {
-        return value;
-      }
-      return value.substring(0, max_length) + "...";
     }
   }
 }
@@ -228,7 +141,6 @@ export default {
 #main {
   background-color: var(--tg-theme-bg-color, white);
   color: var(--tg-theme-text-color, black);
-  /*https://stackoverflow.com/questions/1165497/how-to-prevent-text-from-overflowing-in-css*/
   word-wrap: break-word;
 }
 </style>
