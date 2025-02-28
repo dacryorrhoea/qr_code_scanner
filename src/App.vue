53a8b331334cd3e9825fb34345d8edc7ee024fc7
dacryorrhoea <dacryorrhoea@gmail.com>
2025-02-28 11:12:31 -0800

    pip

--- src/App.vue
+++ src/App.vue
@@ -132,6 +132,13 @@ export default {
     },
     // QR scanner functions
     showQRScanner() {
+      // Sets QR message
+      let par = {
+        text: ""
+      };
+      if (this.is_continuous_scan) {
+        par['text'] = "Continuous scan enabled.";
+      }
       this.TMA.showScanQrPopup(par);
     },
     processQRCode(data) {
