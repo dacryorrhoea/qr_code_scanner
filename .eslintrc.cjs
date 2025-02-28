cf0bd0fcf43e29bb673dba6a126e7b61bead1d2c
dacryorrhoea <dacryorrhoea@gmail.com>
2025-02-28 09:09:53 -0800

    pip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
   extends: [
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended'
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        'sourceType': 'script'
-      }
-    }
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint',
-    'react'
   ],
   rules: {
-    'react/react-in-jsx-scope': 0,
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
   }
-};
+}
